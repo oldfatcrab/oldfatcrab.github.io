a2e218f395891d570584775ee8bf0f76a4a67e4a
Golson Xie <oldfatcrab@gmail.com>
2019-01-01 01:24:26 -0800

    footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created by Golson Xie using <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2019</span>
+    <span class="created-by">
+      Created by Golson Xie using <b><a href="https://github.com/akveo/nebular" target="_blank">nebular</a></b> 2019
+    </span>
     <div class="socials">
       <a href="https://github.com/oldfatcrab/" target="_blank" class="ion ion-social-github"></a>
       <a href="https://www.linkedin.com/in/golsonxie/" target="_blank" class="ion ion-social-linkedin"></a>
